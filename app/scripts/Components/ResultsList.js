2dda307be77d5cf874864a0a1c5bd7a7e4d5882a
Shannon Riester <shannonriester@gmail.com>
2016-08-14 20:41:20 -0500

    good stopping point for mobile view styles on resultsList

--- app/scripts/Components/ResultsList.js
+++ app/scripts/Components/ResultsList.js
@@ -9,18 +9,18 @@ export default React.createClass({
     },
     render: function() {
       let backgroundImage = {backgroundImage: 'url(' + this.props.place.imageUrl + ')'};
-      let color;
-      if (this.props.place.yelpRating === 5) {
-        color = {backgroundColor: '#10e471'};
-      } else if (this.props.place.yelpRating === 4) {
-        color = {backgroundColor: 'rgba(#10e471, 0.69)'};
-      } else if (this.props.place.yelpRating === 3) {
-        color = {backgroundColor: '#ebff00'};
-      } else if (this.props.place.yelpRating === 2) {
-        color = {backgroundColor: '#ffa100'};
-      } else {
-        color = {backgroundColor: '#da3400'};
-      }
+      // let color;
+      // if (this.props.place.yelpRating === 5) {
+      //   color = {backgroundColor: '#10e471'};
+      // } else if (this.props.place.yelpRating === 4) {
+      //   color = {backgroundColor: 'rgba(#10e471, 0.69)'};
+      // } else if (this.props.place.yelpRating === 3) {
+      //   color = {backgroundColor: '#ebff00'};
+      // } else if (this.props.place.yelpRating === 2) {
+      //   color = {backgroundColor: '#ffa100'};
+      // } else {
+      //   color = {backgroundColor: '#da3400'};
+      // }
 
       let status = 'closed';
       if (this.props.place.isClosed) {
@@ -36,9 +36,6 @@ export default React.createClass({
       return (
         <li className="result-item-component">
           <figure className="result-image" style={backgroundImage}>
-            <figcaption className="figcaption1">{this.props.place.categories[0]}</figcaption>
-            <figcaption className="figcaption2">{this.props.place.categories[2]}</figcaption>
-            <figcaption className="figcaption3">{status}</figcaption>
           </figure>
           <div className="result-content-container">
             <main className="about-preview">
@@ -47,7 +44,9 @@ export default React.createClass({
             </main>
 
             <div className="star-rating">
-              <i className="star-icon fa fa-star" aria-hidden="true" style={color}></i>
+              <header className="caption">{this.props.place.categories[0][0]}</header>
+              <i className="star-icon fa fa-star" aria-hidden="true"></i>
+              <footer className="caption">{status}</footer>
             </div>
           </div>
         </li>
