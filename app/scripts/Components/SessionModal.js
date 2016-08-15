cdf087322b205169c8e6ee32ece6682a7814fe9c
Shannon Riester <shannonriester@gmail.com>
2016-08-15 09:57:30 -0500

    working login/signup modal, hiding hero, but only on non-modal view

--- app/scripts/Components/SessionModal.js
+++ app/scripts/Components/SessionModal.js
@@ -5,10 +5,10 @@ import _ from 'underscore';
 import store from '../store';
 
 export default React.createClass({
-  loginModal: function(e) {
+  login: function(e) {
     e.preventDefault();
-    this.props.heroToggle();
-
+    // this.props.heroToggle();
+    console.log(this);
     let username = this.refs.username.value;
     username.toLowerCase();
     let password = this.refs.password.value;
@@ -17,7 +17,7 @@ export default React.createClass({
     this.props.hideModal();
     browserHistory.push(`/search/${store.session.get('query')}`);
   },
-  signupModal: function(e) {
+  signup: function(e) {
     e.preventDefault();
 
     let username = this.refs.username.value;
@@ -45,20 +45,20 @@ export default React.createClass({
     console.log('this modal ', this);
     if (this.props.content === 'login') {
       modalContent = (
-        <form className="login-form" onSubmit={this.loginModal}>
+        <form className="login-form" onSubmit={this.login}>
           <label htmlFor="input-username"></label>
           <input className="user-info-input" type="text" placeholder="username" ref="username" role="button" tabIndex="1" />
           <label htmlFor="input-password"></label>
           <input className="user-info-input" type="password" placeholder="password" ref="password" role="button" tabIndex="2" />
           <section className="button-section">
-            <input className="modal-btn" type="submit" value="submit" ref="submit" role="button" tabIndex="3" />
+            <input className="modal-btn" type="submit" value="submit" ref="submit" role="button" tabIndex="3" onSubmit={this.login} onClick={this.login} />
             <input className="cancel modal-btn" type="button" value="cancel" role="button" tabIndex="4" onClick={this.hideModal}/>
           </section>
         </form>
       );
     } else if (this.props.content === 'sign up') {
       modalContent = (
-        <form className="signup-form" onSubmit={this.signupModal}>
+        <form className="signup-form" onSubmit={this.signup}>
           <label htmlFor="input-username"></label>
           <input className="user-info-input" type="text" placeholder="username" ref="username" role="button" tabIndex="1" />
           <label htmlFor="input-password"></label>
@@ -66,7 +66,7 @@ export default React.createClass({
           <label htmlFor="input-confirm-password"></label>
           <input className="user-info-input" type="password" placeholder="password" ref="password2" tabIndex="3" />
           <section className="button-section">
-            <input className="modal-btn" type="submit" value="submit" ref="submit" role="button" tabIndex="4" />
+            <input className="modal-btn" type="submit" value="submit" ref="submit" role="button" tabIndex="4" onSubmit={this.signup} onClick={this.signup}/>
             <input className="cancel modal-btn" type="button" value="cancel" ref="cancel" role="button" tabIndex="5" onClick={this.hideModal}/>
           </section>
         </form>
