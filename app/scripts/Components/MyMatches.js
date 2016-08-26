d4d593a55ddfa63726e0e063e1ba0d99d09cd7df
Shannon Riester <shannonriester@gmail.com>
2016-08-26 13:55:25 -0500

    working slider on profile view for matches

--- app/scripts/Components/MyMatches.js
+++ app/scripts/Components/MyMatches.js
@@ -8,37 +8,55 @@ export default React.createClass({
   getInitialState: function() {
     return {
         users: [],
-        matches: [],
+        matches: this.props.myMatches,
         scroll: '',
+        fetch: true,
+        viewing: 0,
     }
   },
   scrollLeft: function(e) {
     e.preventDefault();
-    this.setState({scroll:'scroll-left'});
+    this.setState({scroll:'slide-out-left'});
+    window.setTimeout(() => {
+      this.setState({
+        scroll: 'slide-in-right',
+        viewing: this.state.viewing + 3,
+      });
+    }, 300);
+  },
+  scrollRight: function(e) {
+    e.preventDefault();
+    this.setState({scroll: 'slide-out-right'});
+    window.setTimeout(() => {
+      this.setState({
+        scroll: 'slide-in-left',
+        viewing: this.state.viewing - 3,
+    });
+    }, 300);
   },
   updateState: function() {
-    // if (this.state.users.length !== this.state.matches.length) {
-    //   this.props.myMatches.forEach((person, i) => {
-    //     store.userCollection.findUser(person).then((response) => {
-    //       this.setState({users: response.toJSON()});
-    //     })
-    //   });
-    // }
+    // console.log(this.state.users.length + ' | ' + this.state.matches.length);
+    // console.log('this.state.users ', this.state.users);
+    // console.log('this.state.matches', this.state.matches);
+    let newPersonArr = [];
+    if (this.state.fetch && (this.state.matches.length === this.state.matches.length)) {
+      // console.log(this.props.myMatches.length);
+      this.props.myMatches.forEach((person, i) => {
+        store.userCollection.findUser(person).then((response) => {
+          newPersonArr.push(response.toJSON()[0]);
+          this.setState({
+            users: newPersonArr,
+            fetch: false,
+          });
+        })
+      });
+    }
   },
   componentWillReceiveProps: function(newProps) {
-    // console.log('receiving props');
     this.setState({matches: newProps.myMatches});
-
-    // console.log(newProps.myMatches);
-    // console.log(this.state.users.length + '|' + newProps.myMatches.length);
-    // if (this.state.users.length !== newProps.myMatches.length) {
-    //   this.props.myMatches.forEach((person, i) => {
-    //     store.userCollection.findUser(person).then((response) => {
-    //       this.setState({users: response.toJSON()});
-    //     })
-    //   });
-    // }
-    this.updateState();
+    if (newProps.myMatches !== this.state.matches) {
+      this.updateState();
+    }
   },
   componentDidMount: function() {
 
@@ -49,26 +67,27 @@ export default React.createClass({
     // let styles = {backgroundImage: `url(${store.entryImages[this.state.images]})`};
     // <Link className="link" to={`/user/${person}`}><img src='#'/></Link>
     // <Link className="link" to={`/user/${person}`}><h3>{person}</h3></Link>
+    // console.log(this);
     if (this.state.users.length) {
       matchPreview = this.state.users.map((person, i) => {
-        // console.log(person);
           return (
-            <div id={this.state.scroll} className="current-match-preview" key={person.userId}>
+            <div className={`current-match-preview`} key={i}>
               <Link className="link" to={`/user/${person.username}`}><h3>{person.username}</h3></Link>
               <Link className="link" to={`/user/${person.username}`}><img className="match-preview-img" src={person.profile.profilePic}/></Link>
             </div>
           );
-
       });
+      matchPreview = matchPreview.slice(this.state.viewing, this.state.viewing + 3);
     }
 
 
     return (
       <div className="my-matches-component">
         <h2>Your Matches</h2>
-        <div className="matches-container">{matchPreview}</div>
+        <div id={this.state.scroll} className="matches-container">{matchPreview}</div>
 
         <button className="matches-next-btn" onClick={this.scrollLeft}>next</button>
+        <button className="matches-next-btn" onClick={this.scrollRight}>previous</button>
       </div>
     );
   }
