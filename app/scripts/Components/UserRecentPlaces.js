55b9d42b1f5ca3a4f35ad84eb5ce726559169b1c
Shannon Riester <shannonriester@gmail.com>
2016-09-26 14:02:46 -0500

    working place heading display on hover over userRecentPlaces

--- app/scripts/Components/UserRecentPlaces.js
+++ app/scripts/Components/UserRecentPlaces.js
@@ -4,19 +4,33 @@ import { Link } from 'react-router';
 import store from '../store';
 
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      showPlaceName: false,
+    }
+  },
+  showPlaceName: function() {
+    this.setState({showPlaceName: true});
+  },
+  hidePlaceName: function() {
+    this.setState({showPlaceName: false});
+  },
   render: function() {
     let url = `${this.props.place.imageUrl}`;
     let styles = {backgroundImage: 'url(' + url + ')'};
+    let placeName;
+    if (this.state.showPlaceName) {
+      placeName = (<h3>{this.props.place.name}</h3>);
+    }
     return (
       <li className="user-recent-places-component">
         <Link className="link" to={`/places/${this.props.place.yelpID}`}>
           <div className="recent-place-image"
                style={styles}
-               onMouseOver={this.showPlace}
-               >
-            <h3>{this.props.place.name}</h3>
+               onMouseOver={this.showPlaceName}
+               onMouseLeave={this.hidePlaceName}>
+               {placeName}
           </div>
-          <p className="snippet-text">{this.props.place.snippetText}</p>
         </Link>
       </li>
     )
